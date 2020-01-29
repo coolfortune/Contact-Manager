819fdb067eb390e77923aed103ea7ef60b71e1d2
Timothy Jinks <root@DESKTOP-IL2SU52.localdomain>
2020-01-28 21:00:58 -0500

    update to frontend

---
+++ frontend/src/LoginPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import PageTitle from '../components/PageTitle';
+import Login from '../components/Login';
+
+const LoginPage = (props) =>
+{
+
+    return(
+        <div>
+            <PageTitle />
+            <Login />
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ frontend/src/PageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function PageTitle()
+{
+    return(
+        <h1 id="title">Contacto</h1>
+    );
+};
+
+export default PageTitle;
