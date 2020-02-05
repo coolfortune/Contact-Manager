35db191a845966afa54e83d61e968f35579b080c
Javier Aguilar <javierene1064@gmail.com>
2020-02-05 17:36:32 -0500

    working on contactList

--- frontend/src/components/ContactList.js
+++ frontend/src/components/ContactList.js
@@ -1,51 +1,59 @@
-import React, { Component } from 'react'
-import Contact from './Contact.js'
-import '../styles.css';
+import React, { Component } from "react";
+import Contact from "./Contact.js";
+import "../styles.css";
 
-
-import uuid from 'uuid';
-import PropTypes from 'prop-types';
-
+import uuid from "uuid";
+import PropTypes from "prop-types";
 
 
 export class ContactList extends Component {
-
-        // this is hard coded for now but is should be fetched from the database
+    // this is hard coded for now but is should be fetched from the database
+    componentDidMount(){
+        fetch('http://localhost:5000/api/users/login',
+        { method: 'GET',headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(contacts),
+        })
+        .then((respone) =>  respone.json())
+        .then((contacts) => {
+            console.log(contacts);
+        })
+        .catch((error) => {
+            console.error('Error',error);
+        })
+    }
     state = {
         contacts: [
             {
-                id: uuid.v4(),
-                firstName: 'Magic',
-                lastName: 'Johnson'
-            },
-            {
-                id: uuid.v4(),
-                firstName: 'Shaquille',
-                lastName: 'Oneal'
-            },
-            {
-                id: uuid.v4(),
-                firstName: 'Phil',
-                lastName: 'Jackson'
-            }
-        ]
-    }
+        id: uuid.v4(),
+        firstName: "Magic",
+        lastName: "Johnson"
+      },
+      {
+        id: uuid.v4(),
+        firstName: "Shaquille",
+        lastName: "Oneal"
+      },
+      {
+        id: uuid.v4(),
+        firstName: "Phil",
+        lastName: "Jackson"
+      }
+    ]
+  };
 
-    render() {
-        return this.state.contacts.map((contact) => (
-            <div className="ShowList">
-                <Contact key={contact.id} contact={contact} 
-                />
-            </div>
-        ));
-           
-        
-    }
+  render() {
+    return this.state.contacts.map(contact => (
+      <div className="ShowList">
+        <Contact key={contact.id} contact={contact} />
+      </div>
+    ));
+  }
 }
 
 ContactList.propTypes = {
-    contacts: PropTypes.array.isRequired
-}
+  contacts: PropTypes.array.isRequired
+};
 
-
-export default ContactList
+export default ContactList;
