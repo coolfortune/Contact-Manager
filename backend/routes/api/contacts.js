987c654028c3b14e223c71f673269b275e7077ad
Onur <runocan@outlook.com>
2020-02-06 13:26:34 -0500

    Search done

--- backend/routes/api/contacts.js
+++ backend/routes/api/contacts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Contact = require('../../models/Contact');
+const User = require('../../models/User');
 
 // route - Search api/contacts/:id
 // desc  - Searches for a contact
@@ -10,7 +11,9 @@ router.get('/:id', (req, res) => {
 
 const id = { userId: req.params.id };
 
+
 console.log(id);
+
 
 Contact.find(id)
         .sort({ firstName: -1 })
@@ -18,21 +21,21 @@ Contact.find(id)
         .catch(err => console.log(err))
 })
 
+router.post('/:id/search', (req, res) => {
+    
+    const id = { userId: { $in: req.params.id }};
+    const payload = { ...req.body, ...id};
+    
+    console.log(payload);
+    
+    Contact.find(payload)
+    .then(contacts => res.json(contacts))
+    .catch(err => console.log(err))
+})
+
 // route - Update api/contacts/:id
 // desc  - Updates a contact
 // access - Public
-router.post('/:id', (req, res) => {
-
-const id = { userId: { $in: req.params.id }};
-const payload = { ...req.body, ...id};
-
-console.log(payload);
-
-Contact.find(payload)
-        .then(contacts => res.json(contacts))
-        .catch(err => console.log(err))
-})
-
 router.post('/:id/add', (req,res) => {
 
     const newContact = new Contact ({
